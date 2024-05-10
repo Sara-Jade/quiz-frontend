5a575dfcd8cee076820cf2940b070d8d8ee60d5a
Woohoohoohoo! <wisconsinsara@gmail.com>
2024-05-09 21:19:54 -0700

    Home component should load login only. Nav should have quizList.

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core'
 
 @Component({
-    //template: '<quizForm></quizForm><quizList></quizList>'
-    template: '<login></login><quizForm></quizForm><quizList></quizList>'
+    template: '<login></login>'
 })
 export class HomeComponent {
     
