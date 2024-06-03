24320e69a92981bbcbb36f552568f94cdb7d9d5c
Woohoohoohoo! <wisconsinsara@gmail.com>
2024-06-02 19:49:26 -0700

    Need to retrieve a token from localStorage

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,13 +1,20 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
-  const authToken = localStorage.getItem('token');
-  const authReq = req.clone({
+  let authToken: string | null = 'super secret fake token';
+  let authReq: HttpRequest<unknown> = req.clone({
     headers: req.headers.set('Authorization', `Bearer ${authToken}`)
   });
 
-  // header auth appears under lazy update or headers
-  console.log(authReq.headers) 
+  if (localStorage) {
+    authToken = localStorage.getItem('token');
+    authReq = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${authToken}`)
+    });
+    
+    // header auth appears under lazy update or headers
+    console.log(authReq.headers)
+  }
 
   return next(authReq);
 };
